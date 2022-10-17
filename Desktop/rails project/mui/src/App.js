3254a4fc348ccaadd8b56146754536f473e780eb
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-17 23:57:42 +0300

    navbar and its components added

--- Desktop/rails project/mui/src/App.js
+++ Desktop/rails project/mui/src/App.js
@@ -1,14 +1,20 @@
 import Sidebar from './components/Sidebar'
 import Feed from './components/Feed'
 import Rightbar from './components/Rightbar'
+import { Box, Container, Stack } from '@mui/system';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <Feed />
-      <Rightbar />     
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed /> 
+        <Rightbar />
+      </Stack>
+         
+    </Box>
   );
 }
 
--- Desktop/rails project/mui/src/components/Navbar.jsx
+++ Desktop/rails project/mui/src/components/Navbar.jsx
@@ -1,8 +1,81 @@
+import { Mail, Menu, Notifications, Pets } from '@mui/icons-material';
+import { AppBar, Badge, InputBase, styled, Toolbar, Typography, MailIcon, Avatar, MenuItem } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react'
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+
+})); 
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]:{
+    display: "flex"
+  }
+})); 
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]:{
+    display: "none"
+  }
+})); 
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant='h6' sx={{display: {xs: "none", sm: "block"}}}>CAHAYO</Typography>
+        <Pets sx={ { display: { xs: "block", sm: "none" } } } />
+        <Search><InputBase placeholder="search..."/></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{width:30, height:30}} src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=612x612&w=0&h=Ov2YWXw93vRJNKFtkoFjnVzjy_22VcLLXZIcAO25As4='/>
+        </Icons>
+        <UserBox>
+          <Avatar sx={ { width: 30, height: 30 } } src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=612x612&w=0&h=Ov2YWXw93vRJNKFtkoFjnVzjy_22VcLLXZIcAO25As4=' />
+          <Typography variant='h6' sx={{display: {xs: "none", sm: "block"}}}>Earnest</Typography>
+        </UserBox>
+      </StyledToolbar> 
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
   )
 }
 
